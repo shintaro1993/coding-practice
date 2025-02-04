b3b01305ace0d687c66de9d48e5842a84ddab3de
Shintaro Fujiwara <sfujiwara1993@gmail.com>
2025-02-04 22:05:57 +0900

    step2

--- grind41/valid_parentheses.py
+++ grind41/valid_parentheses.py
@@ -1,17 +1,22 @@
+""" Step2
+
+ˆÙí‚È’l‚ª“ü‚Á‚Ä‚«‚½‚Æ‚«‚É‚Ç‚¤‚·‚é‚©l‚¦‚é•È‚ð‚Â‚¯‚é
+
+"""
+
 class Solution:
-    def isvalid(self, s: str) -> bool:
-        parentheses = {')': '(', '}': '{', ']': '['}
-        open_brackets = ['(', '{', '[']
+    def isValid(self, s:str) -> bool:
+        open_to_close_of_same_prarentheses = {'(': ')', '{': '}', '[': ']'}
         stack = []
         for c in s:
-            if c in open_brackets:
+            if c in open_to_close_of_same_parentheses:
                 stack.append(c)
-            else:
-                if not stack:
-                    return False
-                if stack[-1] != parentheses[c]:
-                    return False
-                stack.pop()
+                continue
+            if not stack:
+                return False
+            if c != open_to_close_of_same_parentheses[stack[-1]]:
+                return False
+            stack.pop()
         return not stack
-            
 
+            
