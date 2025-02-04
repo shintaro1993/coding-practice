257a1503a35b5923b37f90d507a79ea371f23349
Shintaro Fujiwara <sfujiwara1993@gmail.com>
2025-02-04 20:11:49 +0900

    change variable name

--- grind41/two_sum.py
+++ grind41/two_sum.py
@@ -1,14 +1,14 @@
-"""Step1
-
+"""Step2
+変数名を改善する
 
 """
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = dict()
+        num_to_index = {}
         for i, num in enumerate(nums):
-            other_num = target - num
-            if other_num in seen:
-                return [i, seen[other_num]]
-            seen[num] = i
+            other_num = target - i
+            if other_num in num_to_dict:
+                return [i, num_to_index[other_num]]
+            num_to_index[num] = i
         return None
